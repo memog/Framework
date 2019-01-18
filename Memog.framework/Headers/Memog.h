79768db570047bb6ffa5a02c13e9161d26d53940
Daniel Garcia <guillermo.garcia@inmediatum.com>
2019-01-17 19:27:36 -0600

    files

---
+++ Memog.framework/Headers/Memog.h
@@ -0,0 +1,19 @@
+//
+//  Memog.h
+//  Memog
+//
+//  Created by Guillermo Garcia on 1/17/19.
+//  Copyright © 2019 Guillermo Garcia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Memog.
+FOUNDATION_EXPORT double MemogVersionNumber;
+
+//! Project version string for Memog.
+FOUNDATION_EXPORT const unsigned char MemogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Memog/PublicHeader.h>
+
+
